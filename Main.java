b28bd8500fe9c0d0c25a7b6f4f6ba7cba5e9e682
lmf <1151011516@qq.com>
2026-01-09 11:19:23 +0800

    up

--- Main.java
+++ Main.java
@@ -11,45 +11,78 @@ public class Main {
 }
 
 
-// 优先队列，大根堆
-
 class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        int n = nums.length;
-        int res[] = new int[n - k + 1];
-        PriorityQueue<int[]> dq = new PriorityQueue<int[]>(
-            new Comparator<int[]>() {
 
-                public int compare(int[] pair1, int[] pair2) {
-                    if (pair1[0] == pair2[0]) {
-                        return pair2[1] - pair1[1];
-                    } else {
-                        return pair2[0] - pair1[0];
-                    }
+    Map<Character, Integer> tar = new HashMap<Character, Integer>();
+    Map<Character, Integer> now = new HashMap<Character, Integer>();
 
-
-                }
-            } 
-
-        );
-
-        for (int i = 0; i < k; i++) {
-            dq.offer(new int[]{nums[i], i});
-        }
-        res[0] = dq.peek()[0];
-
-
-        for (int i = k; i < n; i++) {
-            if (i - dq.peek()[1] >= k ) {
-                dq.poll();
-            }
-            
-            dq.offer(new int[]{nums[i], i});
-            res[i - k + 1] = dq.peek()[0];
-            System.out.println("nums:" + dq.peek()[0] + " i:" + dq.peek()[1]);
-        }
+    public String minWindow(String s, String t) {
         
-        return res;
-    }
-}
+        int tLen = t.length();
+        int sLen = s.length();
+        if (sLen < tLen) {
+            return "";
+        }
 
+        for (int i = 0; i < tLen; i++) {
+            char c1 = t.charAt(i);
+            // 标准的放字符到哈希表
+            tar.put(c1, tar.getOrDefault(c1, 0) + 1);
+            char c2 = s.charAt(i);
+            tar.put(c2, now.getOrDefault(c2, 0) + 1);
+        }
+
+
+        int l = 0;
+        int r = tLen - 1;
+        int ansL = -1;
+        int ansR = -1;
+        int res = 100001;
+
+        while (r < sLen && l < sLen - tLen) {
+
+            // 不符合窗口
+            if (check() && r < sLen - 1) {
+                r++;
+                char c2 = s.charAt(r);
+                tar.put(c2, now.getOrDefault(c2, 0) + 1);
+
+
+            } else {  
+                // 符合窗口
+
+                if (r - l + 1 < res) {
+                    ansL = l;
+                    ansR = r;
+                    res = r - l + 1;
+                }
+                char c2 = s.charAt(l);
+                tar.put(c2, now.getOrDefault(c2, 0) - 1);
+                l++;
+                
+
+            }
+
+        }
+        if (res == 100001) {
+            return "";
+        } else {
+            return s.substring(ansL,ansR);
+        }
+
+    }
+
+    // 下面这一段第一次写的时候完全不会写，然后ai生成了一个看得懂的版本
+    public boolean check() {
+        for (Map.Entry<Character, Integer> entry : tar.entrySet()) {
+            Character key = entry.getKey();
+            Integer val = entry.getValue();
+            if (now.getOrDefault(key, 0) < val) {
+                return false;
+            }
+        
+        }
+        return true;
+    }
+
+}

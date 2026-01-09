3e2c8d9de444ef7faa6c8add00f73adbe7073647
“lmf” <1151011516@qq.com>
2026-01-09 19:13:15 +0800

    init project

---
+++ Hello.java
@@ -0,0 +1,10 @@
+
+// 中文测试
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("hello,world~");
+	}
+
+}
---
+++ code15.java
@@ -0,0 +1,12 @@
+// 双指针法  二重循环，对于每个数，剩下两个数用双指针去找
+// 首先要排序升序，和应该是-num[i]
+// 左指针和右指针加起来如果比-num[i]大，那应该右指针左移
+// 如果小，那应该左指针右移
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        
+    	while 
+
+
+    }
+}
